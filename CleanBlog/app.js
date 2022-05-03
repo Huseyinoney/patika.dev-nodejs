3f3a0b026030b99806412250b0c09acaa83474ba
HUSEYIN <87951691+Huseyinoney@users.noreply.github.com>
2022-05-04 02:58:20 +0300

    Add files via upload

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
 
+const ejs = require("ejs");
+
+app.set("view engine" , "ejs");
+
+app.use(express.static("public"));
+
 app.get("/" , (req,res) => {
-    const blog = {
-        id:1,
-        title:"Blog title",
-        description:"Blog description"
-    };
-    res.send(blog);
+   
+    res.render("index");
+
 });
-
-
+app.get("/post" , (req,res) => {
+   
+    res.render("post");
+    
+});
+app.get("/about" , (req,res) => {
+   
+    res.render("about");
+    
+});
+app.get("/add_post" , (req,res) => {
+   
+    res.render("add_post");
+    
+});
 const port = 3000;
 app.listen(port , () =>{
     console.log("Sunucu %d portunda çalışıyor",port);
